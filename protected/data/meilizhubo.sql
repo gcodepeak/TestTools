35cf54d64b9f7e94e97c0b391f807be0027664d2
wangming <gcodepeak@gmail.com>
2014-01-17 23:36:30 +0800

    update sql schema

--- protected/data/meilizhubo.sql
+++ protected/data/meilizhubo.sql
@@ -4,14 +4,25 @@ use meilizhubo;
 -- Table structure for table `zhubo`
 --
 
-DROP TABLE IF EXISTS `zhubo`;
+/*DROP TABLE IF EXISTS `user`;*/
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `username` varchar(64),
+  `password` varchar(64),
+  `register_time` datetime,
+)
+ENGINE=InnoDB,
+DEFAULT CHARSET=utf8,
+COMMENT = 'the user';
+
+/*DROP TABLE IF EXISTS `zhubo`;*/
 CREATE TABLE IF NOT EXISTS `zhubo` (
   `id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `local_id` int unsigned DEFAULT NULL,
   `url` varchar(64) ,
   `name` varchar(64) ,
   `head_img` varchar(128),
-  `site_id` tinyint ,
+  `site_name` varchar(64),
   `level` tinyint ,
   `wealth_level` tinyint ,
   `time_level` tinyint ,
@@ -22,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `zhubo` (
   `age` tinyint unsigned ,
   `hots` int unsigned ,
   `fans` int unsigned ,
-  `tags` varchar(128) ,
+  `orignal_tags` varchar(128) ,
   `news_num` int unsigned ,
   `news_photo_num` int unsigned ,
   `is_live` tinyint ,
@@ -32,3 +43,26 @@ CREATE TABLE IF NOT EXISTS `zhubo` (
 ENGINE=InnoDB,
 DEFAULT CHARSET=utf8,
 COMMENT = 'the zhubo';
+
+/*DROP TABLE IF EXISTS `tag`;*/
+CREATE TABLE IF NOT EXISTS `tag` (
+  `tag_id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `tag_name` varchar(64),
+  `class_1` varchar(32),
+  `class_2` varchar(32),
+)
+ENGINE=InnoDB,
+DEFAULT CHARSET=utf8,
+COMMENT = 'the tag';
+
+/*DROP TABLE IF EXISTS `zhubo_tag`;*/
+CREATE TABLE IF NOT EXISTS `zhubo_tag` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `tag_id` int unsigned,
+  `zhubo_id` int unsigned,
+  `user_id` int unsigned,
+  `tag_time` datetime,
+)
+ENGINE=InnoDB,
+DEFAULT CHARSET=utf8,
+COMMENT = 'the zhubo_tag';
