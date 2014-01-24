da098ed6505b85b2b0621898cb53851f2c1350b2
wangming <gcodepeak@gmail.com>
2014-01-25 05:24:21 +0800

    update top 14

--- protected/views/zhubo/_top_14.php
+++ protected/views/zhubo/_top_14.php
@@ -9,76 +9,76 @@
 <div class="top14">
 	<ul>
 		<li class="head_cube doublesize" style="top: 0px; left: 0px;" data-id="0">
-			<a href="<?php echo $top14s[0]['url'];?>">
+			<a href="<?php echo Yii::app()->createUrl('zhibo/zhibo',array('id'=>$top14s[0]['id'],))?>" target="_blank">
 				<img alt="" src="<?php echo Yii::app()->baseUrl;?>/images/head_cube_double.png">
 			</a>
 		</li>
 		<li class="head_cube" style="top: 0px; left: 300px;" data-id="1">
-			<a href="<?php echo $top14s[1]['url'];?>">
+			<a href="<?php echo Yii::app()->createUrl('zhibo/zhibo',array('id'=>$top14s[1]['id'],))?>" target="_blank">
 			<img alt="" src="<?php echo Yii::app()->baseUrl;?>/images/head_cube.png">
 			</a>
 		</li>
 		<li class="head_cube" style="top: 0px; left: 450px;" data-id="1">
-			<a href="<?php echo $top14s[2]['url'];?>">
+			<a href="<?php echo Yii::app()->createUrl('zhibo/zhibo',array('id'=>$top14s[2]['id'],))?>" target="_blank">
 			<img alt="" src="<?php echo Yii::app()->baseUrl;?>/images/head_cube.png">
 			</a>
 		</li>
 		<li class="head_cube" style="top: 0px; left: 600px;" data-id="1">
-			<a href="<?php echo $top14s[3]['url'];?>">
+			<a href="<?php echo Yii::app()->createUrl('zhibo/zhibo',array('id'=>$top14s[3]['id'],))?>" target="_blank">
 			<img alt="" src="<?php echo Yii::app()->baseUrl;?>/images/head_cube.png">
 			</a>
 		</li>
 		<li class="head_cube doublesize" style="top: 0px; left: 750px;" data-id="1">
-			<a href="<?php echo $top14s[4]['url'];?>">
+			<a href="<?php echo Yii::app()->createUrl('zhibo/zhibo',array('id'=>$top14s[4]['id'],))?>" target="_blank">
 			<img alt="" src="<?php echo Yii::app()->baseUrl;?>/images/head_cube_double.png">
 			</a>
 		</li>
 		<li class="head_cube" style="top: 0px; left: 1050px;" data-id="1">
-			<a href="<?php echo $top14s[5]['url'];?>">
+			<a href="<?php echo Yii::app()->createUrl('zhibo/zhibo',array('id'=>$top14s[5]['id'],))?>" target="_blank">
 			<img alt="" src="<?php echo Yii::app()->baseUrl;?>/images/head_cube.png">
 			</a>
 		</li>
 		
 		<!-- 第二行 -->
 		<li class="head_cube" style="top: 115px; left: 300px;" data-id="1">
-			<a href="<?php echo $top14s[6]['url'];?>">
+			<a href="<?php echo Yii::app()->createUrl('zhibo/zhibo',array('id'=>$top14s[6]['id'],))?>" target="_blank">
 			<img alt="" src="<?php echo Yii::app()->baseUrl;?>/images/head_cube.png">
 			</a>
 		</li>
 		<li class="head_cube doublesize" style="top: 115px; left: 450px;" data-id="2">
-			<a href="<?php echo $top14s[7]['url'];?>">
+			<a href="<?php echo Yii::app()->createUrl('zhibo/zhibo',array('id'=>$top14s[7]['id'],))?>" target="_blank">
 			<img alt="" src="<?php echo Yii::app()->baseUrl;?>/images/head_cube_double.png">
 			</a>
 		</li>
 		<li class="head_cube" style="top: 115px; left: 1050px;" data-id="1">
-			<a href="<?php echo $top14s[8]['url'];?>">
+			<a href="<?php echo Yii::app()->createUrl('zhibo/zhibo',array('id'=>$top14s[8]['id'],))?>" target="_blank">
 			<img alt="" src="<?php echo Yii::app()->baseUrl;?>/images/head_cube.png">
 			</a>
 		</li>
 		
 		<!-- 第三行 -->
 		<li class="head_cube" style="top: 230px; left: 0px;" data-id="1">
-			<a href="<?php echo $top14s[9]['url'];?>">
+			<a href="<?php echo Yii::app()->createUrl('zhibo/zhibo',array('id'=>$top14s[9]['id'],))?>" target="_blank">
 			<img alt="" src="<?php echo Yii::app()->baseUrl;?>/images/head_cube.png">
 			</a>
 		</li>
 		<li class="head_cube" style="top: 230px; left: 150px;" data-id="1">
-			<a href="<?php echo $top14s[9]['url'];?>">
+			<a href="<?php echo Yii::app()->createUrl('zhibo/zhibo',array('id'=>$top14s[9]['id'],))?>" target="_blank">
 			<img alt="" src="<?php echo Yii::app()->baseUrl;?>/images/head_cube.png">
 			</a>
 		</li>
 		<li class="head_cube" style="top: 230px; left: 300px;" data-id="1">
-			<a href="<?php echo $top14s[9]['url'];?>">
+			<a href="<?php echo Yii::app()->createUrl('zhibo/zhibo',array('id'=>$top14s[1]['id'],))?>" target="_blank">
 			<img alt="" src="<?php echo Yii::app()->baseUrl;?>/images/head_cube.png">
 			</a>
 		</li>
 		<li class="head_cube" style="top: 230px; left: 750px;" data-id="1">
-			<a href="<?php echo $top14s[9]['url'];?>">
+			<a href="<?php echo Yii::app()->createUrl('zhibo/zhibo',array('id'=>$top14s[9]['id'],))?>" target="_blank">
 			<img alt="" src="<?php echo Yii::app()->baseUrl;?>/images/head_cube.png">
 			</a>
 		</li>
 		<li class="head_cube" style="top: 230px; left: 900px;" data-id="1">
-			<a href="<?php echo $top14s[9]['url'];?>">
+			<a href="<?php echo Yii::app()->createUrl('zhibo/zhibo',array('id'=>$top14s[9]['id'],))?>" target="_blank">
 			<img alt="" src="<?php echo Yii::app()->baseUrl;?>/images/head_cube.png">
 			</a>
 		</li>
