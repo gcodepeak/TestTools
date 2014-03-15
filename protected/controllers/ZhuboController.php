989a912de201e417e56e14dc2a6650af24bf0205
wangming <gcodepeak@gmail.com>
2014-03-16 00:14:57 +0800

    fix bug

--- protected/controllers/ZhuboController.php
+++ protected/controllers/ZhuboController.php
@@ -30,7 +30,7 @@ class ZhuboController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view','homepage','jingtiaoxixuan','zuijiaxinren'),
+				'actions'=>array('index','view','homepage','jingtiaoxixuan','zuijiaxinren','top14'),
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
