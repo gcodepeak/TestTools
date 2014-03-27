ccf55148a4d00bab9d2897ef612fa18fb820a7f3
wangming <gcodepeak@gmail.com>
2014-03-27 10:50:16 +0800

    fix

--- js/homepage.js
+++ js/homepage.js
@@ -317,17 +317,17 @@ $(document).ready(function(){
         });
         $(".icon_qq" ).click(function() {
         	//以下为按钮点击事件的逻辑。注意这里要重新打开窗口
-        	   //否则后面跳转到QQ登录，授权页面时会直接缩小当前浏览器的窗口，而不是打开新窗口
-        	   var A=window.open("oauth/index.php","TencentLogin",
+        	//否则后面跳转到QQ登录，授权页面时会直接缩小当前浏览器的窗口，而不是打开新窗口
+        	/*   var A=window.open("oauth/index.php","TencentLogin",
         	   "width=450,height=320,menubar=0,scrollbars=1,
         	   resizable=1,status=1,titlebar=0,toolbar=0,location=1");
-        	/*
+        	*/
         	QC.Login.showPopup({
-    		   appId:"101029271",
+    		   appId:"101047673",
     		   redirectURI:"http://www.meilizhubo.com"
     		});
         	return;
-        	*/
+        	
         	
             var html = $("#login_content").html();
             loginDiv = dialog(html);
